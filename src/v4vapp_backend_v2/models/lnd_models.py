0dd795c9967dec818f92e0c94575e4ef19d20697
Brian of London (home imac) <brian@v4v.app>
2025-02-13 14:45:48 +0200

    feat: refactor LNDInvoice to inherit from Invoice model and streamline attributes
    Signed-off-by: Brian of London (home imac) <brian@v4v.app>

--- src/v4vapp_backend_v2/models/lnd_models.py
+++ src/v4vapp_backend_v2/models/lnd_models.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from typing import Any, Callable, List
 
 from pydantic import BaseModel
+from v4vapp_backend_v2.models.invoice_models import Invoice
 
 from v4vapp_backend_v2.config.setup import LoggerFunction
 
@@ -11,35 +12,8 @@ from v4vapp_backend_v2.config.setup import LoggerFunction
 LND_INVOICE_TAG = r"(.*)(#(v4vapp))"
 
 
-class LNDInvoice(BaseModel):
-    """Model of an LND Invoice"""
+class LNDInvoice(Invoice):
 
-    memo: str = ""
-    r_preimage: str | None = None
-    r_hash: str
-    value: int = 0
-    value_msat: int = 0
-    settled: bool = False
-    creation_date: datetime
-    settle_date: datetime | None = None
-    payment_request: str | None = None
-    description_hash: str | None = None
-    expiry: int | None = None
-    fallback_addr: str | None = None
-    cltv_expiry: int
-    route_hints: List[dict] | None = None
-    private: bool | None = None
-    add_index: int = 0
-    settle_index: int = 0
-    amt_paid: int | None = None
-    amt_paid_sat: int | None = None
-    amt_paid_msat: int | None = None
-    state: str | None = None
-    htlcs: List[dict] | None = None
-    features: dict
-    is_keysend: bool = False
-    payment_addr: str
-    is_amp: bool = False
     is_lndtohive: bool = False
 
     def __init__(__pydantic_self__, **data: Any) -> None:
@@ -72,3 +46,66 @@ class LNDInvoice(BaseModel):
                 "invoice": self.model_dump(exclude_none=True),
             },
         )
+
+
+# class LNDInvoice(BaseModel):
+#     """Model of an LND Invoice"""
+
+#     memo: str = ""
+#     r_preimage: str | None = None
+#     r_hash: str
+#     value: int = 0
+#     value_msat: int = 0
+#     settled: bool = False
+#     creation_date: datetime
+#     settle_date: datetime | None = None
+#     payment_request: str | None = None
+#     description_hash: str | None = None
+#     expiry: int | None = None
+#     fallback_addr: str | None = None
+#     cltv_expiry: int
+#     route_hints: List[dict] | None = None
+#     private: bool | None = None
+#     add_index: int = 0
+#     settle_index: int = 0
+#     amt_paid: int | None = None
+#     amt_paid_sat: int | None = None
+#     amt_paid_msat: int | None = None
+#     state: str | None = None
+#     htlcs: List[dict] | None = None
+#     features: dict
+#     is_keysend: bool = False
+#     payment_addr: str
+#     is_amp: bool = False
+#     is_lndtohive: bool = False
+
+#     def __init__(__pydantic_self__, **data: Any) -> None:
+#         super().__init__(**data)
+#         # perform my check to see if this invoice can be paid to Hive
+#         if __pydantic_self__.memo:
+#             match = re.match(LND_INVOICE_TAG, __pydantic_self__.memo.lower())
+#             if match:
+#                 __pydantic_self__.is_lndtohive = True
+
+#     def invoice_message(self) -> str:
+#         if self.settled:
+#             return (
+#                 f"✅ Settled invoice {self.add_index} "
+#                 f"with memo {self.memo} {self.value:,.0f} sats"
+#             )
+#         else:
+#             return (
+#                 f"✅ Valid   invoice {self.add_index} "
+#                 f"with memo {self.memo} {self.value:,.0f} sats"
+#             )
+
+#     def invoice_log(
+#         self, logger_func: LoggerFunction, send_notification: bool = False
+#     ) -> None:
+#         logger_func(
+#             self.invoice_message(),
+#             extra={
+#                 "notification": send_notification,
+#                 "invoice": self.model_dump(exclude_none=True),
+#             },
+#         )
