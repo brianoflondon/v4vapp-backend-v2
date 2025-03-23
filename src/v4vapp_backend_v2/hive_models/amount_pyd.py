f20da308a7941fe8a18ca201b13d4089aa4f0241
Brian of London (home imac) <brian@v4v.app>
2025-03-23 08:42:40 +0200

    refactor: enhance fixed_width_str method documentation and adjust formatting logic
    Signed-off-by: Brian of London (home imac) <brian@v4v.app>

--- src/v4vapp_backend_v2/hive_models/amount_pyd.py
+++ src/v4vapp_backend_v2/hive_models/amount_pyd.py
@@ -32,14 +32,25 @@ class AmountPyd(BaseModel):
         return self.beam.__str__()
 
     def fixed_width_str(self, width: int) -> str:
-        number_str = f"{self.amount_decimal:,.3f}".rjust(width)
+        """
+        Returns a fixed-width string representation of the amount and currency symbol.
+        Args:
+            width (int): The total width of the resulting string.
+        Returns:
+            str: A string containing the amount formatted to three decimal places,
+                 right-justified to the specified width,
+                 followed by the currency symbol right-justified to 4 characters.
+        """
+
+        number_str = f"{self.amount_decimal:,.3f}".rjust(width - 5)
         currency_str = f"{self.symbol:>4}"
         return f"{number_str} {currency_str}"
 
     @property
     def amount_decimal(self) -> float:
         """Convert string amount to decimal with proper precision"""
-        return float(self.amount) / (10**self.precision)
+        # return float(self.amount) / (10**self.precision)
+        return self.beam.amount_decimal
 
     @property
     def symbol(self) -> str:
