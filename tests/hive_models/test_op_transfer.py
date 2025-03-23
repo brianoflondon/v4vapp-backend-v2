513eca2996c984a23a010ae780db8356b738e307
Brian of London (Dot) <brian@v4v.app>
2025-03-23 09:47:35 +0200

    test: add async MongoDB integration test for Transfer model with unique index validation
    Signed-off-by: Brian of London (Dot) <brian@v4v.app>

--- tests/hive_models/test_op_transfer.py
+++ tests/hive_models/test_op_transfer.py
@@ -3,7 +3,12 @@ import os
 from typing import Dict, Generator
 
 import pytest
+from beem.amount import Amount  # type: ignore
+from pymongo.errors import DuplicateKeyError
 
+from tests.config.test_db_config import set_base_config_path
+from tests.database.test_db import drop_collection_and_user
+from v4vapp_backend_v2.database.db import MongoDBClient
 from v4vapp_backend_v2.hive.hive_extras import get_hive_client
 from v4vapp_backend_v2.hive_models.op_transfer import Transfer, TransferRaw
 from v4vapp_backend_v2.hive_models.op_types_enums import OpTypes
@@ -83,3 +88,65 @@ async def test_model_dump_transfer_enhanced():
             assert hive_event_model["memo"] == transfer.memo
             assert transfer.log_str
             assert transfer.notification_str
+            assert (
+                transfer.conv.conv_from
+                == Amount(hive_event_model["amount"]).symbol.lower()
+            )
+
+
+# Test with live MongoDB
+
+
+@pytest.mark.asyncio
+async def test_model_dump_mongodb(set_base_config_path: None):
+    """
+    Test the dumping of the Transfer model into MongoDB.
+    This test performs the following steps:
+    1. Updates the quote for the Transfer model.
+    2. Connects to a MongoDB test client.
+    3. Drops the existing collection if it exists.
+    4. Creates a unique index on the collection.
+    5. Loads hive events of type 'transfer' and validates them against the Transfer model.
+    6. Inserts the validated transfer documents into the collection.
+    7. Asserts that the document was inserted successfully.
+    8. Attempts to insert a duplicate document and expects a DuplicateKeyError.
+    9. Asserts that the document can be found in the collection.
+    10. Asserts that a non-existent document cannot be found.
+    11. Drops the test database.
+    Args:
+        set_base_config_path (None): Fixture to set the base configuration path.
+    """
+    
+    collection_name = "op_transfer"
+    await Transfer.update_quote()
+    async with MongoDBClient(
+        db_conn="conn_1", db_name="test_db", db_user="test_user"
+    ) as test_client:
+        test_client.db[collection_name].drop()
+        index_key = [["trx_id", 1], ["op_in_trx", 1]]
+        test_client.db[collection_name].create_index(
+            keys=index_key, name="timestamp", unique=True
+        )
+        for hive_event in load_hive_events(OpTypes.TRANSFER):
+            if hive_event["type"] == "transfer":
+                transfer = Transfer.model_validate(hive_event)
+                insert_op_transfer = transfer.model_dump(by_alias=True)
+                insert_ans = await test_client.insert_one(
+                    collection_name=collection_name,
+                    document=insert_op_transfer,
+                )
+                assert insert_ans is not None
+                find_one_ans = await test_client.find_one(
+                    collection_name=collection_name, query={"trx_id": transfer.trx_id}
+                )
+                with pytest.raises(DuplicateKeyError) as exc_info:
+                    await test_client.insert_one(
+                        collection_name=collection_name, document=insert_op_transfer
+                    )
+                    print(exc_info)
+            assert find_one_ans is not None
+            find_one_fail_ans = await test_client.find_one(
+                collection_name, {collection_name: "fail"}
+            )
+            assert find_one_fail_ans is None
+        await test_client.drop_database("test_db")
