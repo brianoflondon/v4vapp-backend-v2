5802e16a219a0c277fafb688756eb10fc803ba55
Brian of London (home imac) <brian@v4v.app>
2025-03-23 15:18:52 +0200

    feat: implement AccountWitnessVote model with voter details and validation test
    Signed-off-by: Brian of London (home imac) <brian@v4v.app>

---
+++ src/v4vapp_backend_v2/hive_models/op_account_witness_vote.py
@@ -0,0 +1,84 @@
+from dataclasses import asdict
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+from v4vapp_backend_v2.hive.voting_power import VotingPower
+
+
+class VoterDetails(BaseModel):
+    voter: str
+    proposal: int
+    proposal_total_votes: float
+    vesting_power: float
+    delegated_vesting_power: float
+    received_vesting_power: float
+    vote_value: float
+    proxy_value: float
+    prop_percent: float
+    total_value: float
+    total_percent: float
+
+
+class AccountWitnessVote(BaseModel):
+    op_in_trx: int
+    trx_id: str
+    account: str
+    approve: bool
+    block_num: int
+    timestamp: datetime
+    trx_num: int
+    type: str
+    voter_details: VoterDetails | None = None
+    witness: str
+
+    def __init__(self, **data):
+        super().__init__(**data)
+
+    def get_voter_details(self):
+        voter_power = VotingPower(self.account)
+        self.voter_details = VoterDetails.model_validate(asdict(voter_power))
+
+    @property
+    def log_str(self) -> str:
+        voted_for = "voted for" if self.approve else "unvoted"
+        if self.voter_details:
+            total_value = self.voter_details.total_value
+        else:
+            total_value = 0
+        log_str = (
+            f"👁️ {self.account} "
+            f"{voted_for} {self.witness} "
+            f"with {total_value:,.0f} HP"
+        )
+        return log_str
+
+
+# Example usage
+data = {
+    "_id": "5eaa46458b2fa7b776eaaeb6c1c437027777ae8e",
+    "op_in_trx": 0,
+    "trx_id": "5eaa46458b2fa7b776eaaeb6c1c437027777ae8e",
+    "account": "ladyaryastark",
+    "approve": True,
+    "block_num": 94188656,
+    "timestamp": {"$date": "2025-03-16T14:55:45.000Z"},
+    "trx_num": 16,
+    "type": "account_witness_vote",
+    "vote_value": 241.55820571089652,
+    "voter_details": {
+        "voter": "ladyaryastark",
+        "proposal": 0,
+        "proposal_total_votes": 36456484.28235988,
+        "vesting_power": 241.55820571089652,
+        "delegated_vesting_power": 27.022683551570367,
+        "received_vesting_power": 44.024000911248805,
+        "vote_value": 241.55820571089652,
+        "proxy_value": 0,
+        "prop_percent": 0.0006625932545771528,
+        "total_value": 241.55820571089652,
+        "total_percent": 0.0006625932545771528,
+    },
+    "witness": "enginewitty",
+}
---
+++ tests/hive_models/test_op_account_witness_vote.py
@@ -0,0 +1,34 @@
+from typing import Dict
+
+from tests.hive_models.load_data import load_hive_events
+from tests.hive_models.test_op_account_witness_vote import load_hive_events
+from v4vapp_backend_v2.hive_models.op_account_witness_vote import AccountWitnessVote
+from v4vapp_backend_v2.hive_models.op_types_enums import OpTypes
+
+
+def test_op_account_witness_vote():
+    """
+    Test the validation of the OpAccountWitnessVote model with hive
+        events of type 'account_witness_vote'.
+    This test function performs the following steps:
+    1. Initializes a counter to track the number of 'account_witness_vote' events.
+    2. Iterates through hive events of type 'account_witness_vote' loaded by the `load_hive_events` function.
+    3. For each event of type 'account_witness_vote':
+        - Increments the counter.
+        - Validates the event using the `OpAccountWitnessVote.model_validate` method.
+        - Asserts that the transaction ID (`trx_id`) matches between the event
+                and the validated model.
+        - Asserts that the `vote_value` matches between the event and the validated model.
+        - Prints the voter's name.
+    4. Asserts that the total count of 'account_witness_vote' events is 28.
+    """
+
+    count = 0
+    for hive_event in load_hive_events(OpTypes.ACCOUNT_WITNESS_VOTE):
+        if hive_event["type"] == "account_witness_vote":
+            count += 1
+            account_witness_vote = AccountWitnessVote.model_validate(hive_event)
+            assert account_witness_vote.trx_id == hive_event["trx_id"]
+            account_witness_vote.get_voter_details()
+            assert account_witness_vote.voter_details.voter == hive_event["account"]
+    assert count == 14
