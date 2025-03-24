cfbb557314696bc42be34bf2adca5b99fcc739cc
Brian of London (home imac) <brian@v4v.app>
2025-03-24 13:34:49 +0200

    test: add MongoDB dumping tests for various operation models
    Signed-off-by: Brian of London (home imac) <brian@v4v.app>

---
+++ tests/hive_models/test_all_op_model_dump_mongodb.py
@@ -0,0 +1,137 @@
+import json
+import os
+from pathlib import Path
+from typing import Dict, Generator
+
+import pytest
+from beem.amount import Amount  # type: ignore
+from pymongo.errors import DuplicateKeyError
+
+from tests.load_data import load_hive_events
+from v4vapp_backend_v2.config.setup import InternalConfig
+from v4vapp_backend_v2.database.db import MongoDBClient
+from v4vapp_backend_v2.helpers.general_purpose_funcs import cap_camel_case
+from v4vapp_backend_v2.hive_models.op_account_witness_vote import AccountWitnessVote
+from v4vapp_backend_v2.hive_models.op_fill_order import FillOrder
+from v4vapp_backend_v2.hive_models.op_limit_order_create import LimitOrderCreate
+from v4vapp_backend_v2.hive_models.op_producer_reward import ProducerReward
+from v4vapp_backend_v2.hive_models.op_transfer import Transfer, TransferRaw
+from v4vapp_backend_v2.hive_models.op_types_enums import OpTypes
+
+
+@pytest.fixture()
+def set_base_config_path(monkeypatch: pytest.MonkeyPatch):
+    test_config_path = Path("tests/data/config")
+    monkeypatch.setattr(
+        "v4vapp_backend_v2.config.setup.BASE_CONFIG_PATH", test_config_path
+    )
+    test_config_logging_path = Path(test_config_path, "logging/")
+    monkeypatch.setattr(
+        "v4vapp_backend_v2.config.setup.BASE_LOGGING_CONFIG_PATH",
+        test_config_logging_path,
+    )
+    yield
+
+    # Unpatch the monkeypatch
+    monkeypatch.undo()
+
+
+@pytest.fixture(autouse=True)
+def reset_internal_config(monkeypatch: pytest.MonkeyPatch):
+    # Reset the singleton instance before each test
+    monkeypatch.setattr("v4vapp_backend_v2.config.setup.InternalConfig._instance", None)
+    yield
+    # Reset the singleton instance after each test
+    monkeypatch.setattr("v4vapp_backend_v2.config.setup.InternalConfig._instance", None)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "op_to_test",
+    [
+        {"op_type": OpTypes.TRANSFER, "collection_name": "transfer", "model": Transfer},
+        {
+            "op_type": OpTypes.ACCOUNT_WITNESS_VOTE,
+            "collection_name": "account_witness_vote",
+            "model": AccountWitnessVote,
+        },
+        {
+            "op_type": OpTypes.PRODUCER_REWARD,
+            "collection_name": "producer_reward",
+            "model": ProducerReward,
+        },
+        {
+            "op_type": OpTypes.LIMIT_ORDER_CREATE,
+            "collection_name": "limit_order_create",
+            "model": LimitOrderCreate,
+        },
+        {
+            "op_type": OpTypes.FILL_ORDER,
+            "collection_name": "fill_order",
+            "model": FillOrder,
+        },
+    ],
+    ids=[
+        "test_transfer",
+        "test_account_witness_vote",
+        "test_producer_reward",
+        "test_limit_order_create",
+        "test_fill_order",
+    ],
+)
+async def test_model_dump_mongodb(set_base_config_path, op_to_test):
+    """
+    Test the dumping of the model into MongoDB.
+    This test performs the following steps:
+    1. Updates the quote for the model.
+    2. Connects to a MongoDB test client.
+    3. Drops the existing collection if it exists.
+    4. Creates a unique index on the collection.
+    5. Loads hive events of the specified type and validates them against the model.
+    6. Inserts the validated documents into the collection.
+    7. Asserts that the document was inserted successfully.
+    8. Attempts to insert a duplicate document and expects a DuplicateKeyError.
+    9. Asserts that the document can be found in the collection.
+    10. Asserts that a non-existent document cannot be found.
+    11. Drops the test database.
+    Args:
+        set_base_config_path (None): Fixture to set the base configuration path.
+        op_to_test (dict): Dictionary containing the operation type and collection name.
+    """
+
+    InternalConfig()
+    # collection_name = op_to_test["collection_name"]
+    collection_name = "all_ops"
+    op_type = op_to_test["op_type"]
+    if op_type == OpTypes.TRANSFER:
+        await Transfer.update_quote()
+    async with MongoDBClient(
+        db_conn="conn_1", db_name="test_db", db_user="test_user"
+    ) as test_client:
+        test_client.db[collection_name].drop()
+        index_key = [["trx_id", 1], ["op_in_trx", 1], ["block_num", 1]]
+        test_client.db[collection_name].create_index(
+            keys=index_key, name="timestamp", unique=True
+        )
+        for hive_event in load_hive_events(op_type):
+            if hive_event["type"] == op_type.value:
+                model_class = op_to_test["model"]
+                model_instance = model_class.model_validate(hive_event)
+                insert_op = model_instance.model_dump(by_alias=True)
+                insert_ans = await test_client.insert_one(
+                    collection_name=collection_name,
+                    document=insert_op,
+                )
+                assert insert_ans is not None
+                find_one_ans = await test_client.find_one(
+                    collection_name=collection_name,
+                    query={"trx_id": model_instance.trx_id},
+                )
+                assert find_one_ans is not None
+                with pytest.raises(DuplicateKeyError) as exc_info:
+                    await test_client.insert_one(
+                        collection_name=collection_name, document=insert_op
+                    )
+                    print(exc_info)
+
+        await test_client.drop_database("test_db")
