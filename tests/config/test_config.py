f388a3877b23910bb586ea590941440575f3cb68
Brian of London (home imac) <brian@v4v.app>
2025-04-22 11:46:14 +0300

    feat: refactor test fixtures and add alternate and bad config files
    Signed-off-by: Brian of London (home imac) <brian@v4v.app>

--- tests/config/test_config.py
+++ tests/config/test_config.py
@@ -3,16 +3,32 @@ from pathlib import Path
 import pytest
 from yaml import safe_load
 
-from v4vapp_backend_v2.config.setup import (
-    Config,
-    InternalConfig,
-    NotificationBotConfig,
-    StartupFailure,
-)
+from v4vapp_backend_v2.config.setup import Config, InternalConfig, StartupFailure
+
+# @pytest.fixture(autouse=True)
+# def set_base_config_path(monkeypatch: pytest.MonkeyPatch):
+#     test_config_path = Path("tests/data/config")
+#     monkeypatch.setattr("v4vapp_backend_v2.config.setup.BASE_CONFIG_PATH", test_config_path)
+#     test_config_logging_path = Path(test_config_path, "logging/")
+#     monkeypatch.setattr(
+#         "v4vapp_backend_v2.config.setup.BASE_LOGGING_CONFIG_PATH",
+#         test_config_logging_path,
+#     )
+#     yield
+#     # No need to restore the original value, monkeypatch will handle it
+
+
+# @pytest.fixture(autouse=True)
+# def reset_internal_config(monkeypatch: pytest.MonkeyPatch):
+#     # Reset the singleton instance before each test
+#     monkeypatch.setattr("v4vapp_backend_v2.config.setup.InternalConfig._instance", None)
+#     yield
+#     # Reset the singleton instance after each test
+#     monkeypatch.setattr("v4vapp_backend_v2.config.setup.InternalConfig._instance", None)
 
 
 @pytest.fixture(autouse=True)
-def set_base_config_path(monkeypatch: pytest.MonkeyPatch):
+def set_base_config_path_combined(monkeypatch: pytest.MonkeyPatch):
     test_config_path = Path("tests/data/config")
     monkeypatch.setattr("v4vapp_backend_v2.config.setup.BASE_CONFIG_PATH", test_config_path)
     test_config_logging_path = Path(test_config_path, "logging/")
@@ -20,20 +36,14 @@ def set_base_config_path(monkeypatch: pytest.MonkeyPatch):
         "v4vapp_backend_v2.config.setup.BASE_LOGGING_CONFIG_PATH",
         test_config_logging_path,
     )
-    yield
-    # No need to restore the original value, monkeypatch will handle it
-
-
-@pytest.fixture(autouse=True)
-def reset_internal_config(monkeypatch: pytest.MonkeyPatch):
-    # Reset the singleton instance before each test
     monkeypatch.setattr("v4vapp_backend_v2.config.setup.InternalConfig._instance", None)
     yield
-    # Reset the singleton instance after each test
-    monkeypatch.setattr("v4vapp_backend_v2.config.setup.InternalConfig._instance", None)
+    monkeypatch.setattr(
+        "v4vapp_backend_v2.config.setup.InternalConfig._instance", None
+    )  # Resetting InternalConfig instance
 
 
-def test_valid_config_file_and_model_validate(set_base_config_path: None):
+def test_valid_config_file_and_model_validate():
     config_file = Path("tests/data/config", "config.yaml")
     with open(config_file) as f_in:
         raw_config = safe_load(f_in)
@@ -47,7 +57,7 @@ def test_valid_config_file_and_model_validate(set_base_config_path: None):
         assert False
 
 
-def test_internal_config(set_base_config_path: None):
+def test_internal_config():
     config_file = Path("tests/data/config", "config.yaml")
     with open(config_file) as f_in:
         raw_config = safe_load(f_in)
@@ -73,22 +83,18 @@ def test_internal_config(set_base_config_path: None):
         int_config.lnd_connections["bad_example"]
 
 
-def test_singleton_config(set_base_config_path: None):
+def test_singleton_config():
     internal_config = InternalConfig()
     internal_config2 = InternalConfig()
     assert internal_config is internal_config2
 
 
 def test_bad_internal_config(monkeypatch: pytest.MonkeyPatch):
-    test_config_path_bad = Path("tests/data/config-bad")
-    monkeypatch.setattr("v4vapp_backend_v2.config.setup.BASE_CONFIG_PATH", test_config_path_bad)
-    # detect sys.exit(1) call
-
     with pytest.raises(StartupFailure):
-        InternalConfig()
+        InternalConfig(config_filename="bad.config.yaml")
 
 
-def test_notification_bot_config(set_base_config_path: None):
+def test_notification_bot_config():
     internal_config = InternalConfig()
     assert internal_config.config is not None
     assert internal_config.config.notification_bots is not None
@@ -97,7 +103,7 @@ def test_notification_bot_config(set_base_config_path: None):
         print(internal_config.config.notification_bots[bot].token)
 
 
-def test_notification_bot_find_bot_name(set_base_config_path: None):
+def test_notification_bot_find_bot_name():
     internal_config = InternalConfig()
     bot_name = internal_config.config.find_notification_bot_name(
         "1234567890:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"
@@ -105,12 +111,28 @@ def test_notification_bot_find_bot_name(set_base_config_path: None):
     assert bot_name == "second-bot"
 
 
-def test_hive_acc_name(set_base_config_path: None):
+def test_hive_acc_name():
     internal_config = InternalConfig()
     hive_accs = internal_config.config.hive.hive_accs
     assert hive_accs is not None
     hive_acc = hive_accs["someaccount"]
     assert hive_acc.name == "someaccount"
+
+
+def test_alternate_config_file():
+    config_file = Path("tests/data/config", "alternate.config.yaml")
+    with open(config_file) as f_in:
+        raw_config = safe_load(f_in)
+    assert raw_config is not None
+
+    try:
+        ic = InternalConfig(config_filename="alternate.config.yaml")
+        config = Config.model_validate(raw_config)
+        assert ic.config.logging == config.logging
+        assert config is not None
+    except Exception as e:
+        print(e)
+        assert False
 
 
 # @pytest.mark.skip(reason="Not implemented yet")
