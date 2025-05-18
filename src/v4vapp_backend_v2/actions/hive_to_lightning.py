60c6c20747421b433a00a0476d31ca69fdf75c4b
Brian of London (home imac) <brian@v4v.app>
2025-05-18 09:36:52 +0300

    feat: Enhance error handling and refactor payment request decoding in Hive to Lightning processing
    Signed-off-by: Brian of London (home imac) <brian@v4v.app>

--- src/v4vapp_backend_v2/actions/hive_to_lightning.py
+++ src/v4vapp_backend_v2/actions/hive_to_lightning.py
@@ -5,6 +5,15 @@ from v4vapp_backend_v2.helpers.crypto_prices import Currency
 from v4vapp_backend_v2.hive_models.op_all import OpAny
 from v4vapp_backend_v2.hive_models.op_transfer import TransferBase
 from v4vapp_backend_v2.lnd_grpc.lnd_client import LNDClient
+from v4vapp_backend_v2.models.pay_req import PayReq
+
+
+class HiveToLightningError(Exception):
+    """
+    Custom exception for Hive to Lightning errors.
+    """
+
+    pass
 
 
 async def check_for_hive_to_lightning(op: OpAny) -> bool:
@@ -28,10 +37,12 @@ async def process_hive_to_lightning(op: TransferBase) -> None:
         or not lnd_config.default
         or not hive_config.server_account
     ):
-        logger.warning("Hive or LND configuration is missing.")
-        return
-    server_account = hive_config.server_account.name
+        # Log a warning if the configuration is missing
+        message = f"Missing configuration details for Hive or LND: {hive_config}, {lnd_config}"
+        logger.warning(message, extra={"notification": False})
+        raise HiveToLightningError(message)
 
+    server_account = hive_config.server_account.name
     if op.to_account == server_account:
         # Process the operation
         if await check_for_hive_to_lightning(op):
@@ -40,36 +51,53 @@ async def process_hive_to_lightning(op: TransferBase) -> None:
                 f"Processing operation to {server_account} ({op.from_account} -> {op.to_account})",
                 extra={"notification": False, "op": op.model_dump()},
             )
-            lnd_client = LNDClient(connection_name=lnd_config.default)
-            pay_req = await decode_any_lightning_string(input=op.d_memo, lnd_client=lnd_client)
-            if pay_req and pay_req.value:
-                # This is where we will check the value of the invoice, the amount of Hive sent
-                # The fee and the usage of the user.
-                logger.info(
-                    f"Decoded Lightning invoice: {pay_req}",
-                    extra={"notification": False, "op": op.model_dump()},
-                )
-                sats_to_send = pay_req.value
-                send_conversion = CryptoConversion(conv_from=Currency.SATS, value=sats_to_send)
-                await send_conversion.get_quote()
-                send_conv = send_conversion.conversion
-                if send_conv.in_limits:
-                    logger.info(
-                        f"Conversion details {send_conv}",
+            if op.memo:
+                try:
+                    pay_req = await decode_message(op.memo)
+                    if pay_req:
+                        pass
+                    else:
+                        logger.warning(
+                            "Failed to decode Lightning invoice",
+                            extra={"notification": False, "op": op.model_dump()},
+                        )
+                except Exception as e:
+                    logger.error(
+                        f"Error decoding Lightning invoice: {e}",
                         extra={"notification": False, "op": op.model_dump()},
                     )
-                logger.info(
-                    f"Conversion details {send_conv}", extra={"send_conv": send_conv.model_dump()}
-                )
+                    raise HiveToLightningError(f"Error decoding Lightning invoice: {e}")
             else:
                 logger.warning(
                     "Failed to decode Lightning invoice",
                     extra={"notification": False, "op": op.model_dump()},
                 )
 
-        # first double check this is a transfer to the server account
-    else:
-        logger.warning(
-            f"Operation to sent to {server_account} ({op.from_account} -> {op.to_account})",
-            extra={"notification": False, "op": op.model_dump()},
+
+
+async def decode_message(message: str) -> PayReq | None:
+    """
+    Process the payment request.
+    """
+    # Placeholder for actual implementation
+    lnd_config = InternalConfig().config.lnd_config
+    logger.info(f"Processing payment request: {message}")
+    lnd_client = LNDClient(connection_name=lnd_config.default)
+    pay_req = await decode_any_lightning_string(input=message, lnd_client=lnd_client)
+    if pay_req and pay_req.value:
+        # This is where we will check the value of the invoice, the amount of Hive sent
+        # The fee and the usage of the user.
+        logger.info(
+            f"Decoded Lightning invoice: {pay_req}",
+            extra={"notification": False, "pay_req": pay_req.model_dump()},
         )
+        sats_to_send = pay_req.value
+        send_conversion = CryptoConversion(conv_from=Currency.SATS, value=sats_to_send)
+        await send_conversion.get_quote()
+        pay_req.conv = send_conversion.conversion
+        if pay_req.conv.in_limits:
+            logger.info(
+                f"Pay request decoded {pay_req.memo}", extra={"pay_req": pay_req.model_dump()}
+            )
+            return pay_req
+    return None
