a38b41a66d711a9bdf458bc13ef97b3bc9764e3b
Brian of London (home imac) <brian@v4v.app>
2025-07-10 12:18:16 +0300

    feat: enhance main function to include keepsats balance retrieval and withdrawal ledger entry creation
    Signed-off-by: Brian of London (home imac) <brian@v4v.app>

--- src/jupyter/check_limits.py
+++ src/jupyter/check_limits.py
@@ -1,16 +1,21 @@
 import asyncio
 import os
-from datetime import timedelta
+from datetime import datetime, timedelta, timezone
 from pprint import pprint
 
 from v4vapp_backend_v2.accounting.account_balances import (
     check_hive_conversion_limits,
+    get_account_balance_printout,
     get_account_lightning_conv,
     get_keepsats_balance,
 )
+from v4vapp_backend_v2.accounting.ledger_account_classes import AssetAccount, LiabilityAccount
+from v4vapp_backend_v2.accounting.ledger_entry import LedgerEntry, LedgerType
 from v4vapp_backend_v2.actions.tracked_models import TrackedBaseModel
 from v4vapp_backend_v2.config.setup import InternalConfig
 from v4vapp_backend_v2.database.db import get_mongodb_client_defaults
+from v4vapp_backend_v2.helpers.crypto_conversion import CryptoConversion
+from v4vapp_backend_v2.helpers.crypto_prices import Currency
 
 
 async def main():
@@ -39,9 +44,46 @@ async def main():
     limit_ok = all(limit.limit_ok for limit in limits)
     print("All limits OK:", limit_ok)
 
-    keepsats_balance = await get_keepsats_balance(cust_id=cust_id)
+    print("-------------- Keepsats balance ----------------")
+    keepsats_balance = await get_keepsats_balance(cust_id=cust_id, line_items=True)
     print("Keepsats Balance Summary:")
     pprint(keepsats_balance)
+    for ledger_entry in keepsats_balance.ledger_entries:
+        print(ledger_entry)
+
+    ledger_type = LedgerType.WITHDRAW_KEEPSATS
+    amount_msats = 3_000_000
+
+    debit_conversion = CryptoConversion(conv_from=Currency.MSATS, value=amount_msats)
+    await debit_conversion.get_quote()
+
+    account_printout_str, account_details = await get_account_balance_printout(
+        account=LiabilityAccount(name="Customer Liability", sub=cust_id), line_items=False
+    )
+
+    print(account_printout_str)
+    pprint(account_details)
+
+    withdraw_ledger_entry = LedgerEntry(
+        cust_id=cust_id,
+        ledger_type=ledger_type,
+        group_id=f"test_entry001-{ledger_type.value}",
+        timestamp=datetime.now(tz=timezone.utc),
+        description=f"Withdrawal Keepsats {amount_msats / 1000:,.0f} sats for {cust_id}",
+        debit=LiabilityAccount(
+            name="Customer Liability",
+            sub=cust_id,  # This is the CUSTOMER
+        ),
+        debit_unit=Currency.MSATS,
+        debit_amount=amount_msats,
+        debit_conv=debit_conversion.conversion,
+        credit=AssetAccount(name="Treasury Lightning", sub="keepsats"),
+        credit_unit=Currency.MSATS,
+        credit_amount=amount_msats,
+        credit_conv=debit_conversion.conversion,
+    )
+    print(withdraw_ledger_entry)
+    # await withdraw_ledger_entry.save()
 
 
 if __name__ == "__main__":
