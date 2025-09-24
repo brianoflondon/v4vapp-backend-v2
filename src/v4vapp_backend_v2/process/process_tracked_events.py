96b2172f3e72598c81cf9636ec18abd2d4f541aa
Brian of London (home imac) <brian@v4v.app>
2025-09-24 16:56:23 +0300

    fix: add logging for tracked operation details to improve traceability
    Signed-off-by: Brian of London (home imac) <brian@v4v.app>

--- src/v4vapp_backend_v2/process/process_tracked_events.py
+++ src/v4vapp_backend_v2/process/process_tracked_events.py
@@ -95,6 +95,7 @@ async def process_tracked_event(tracked_op: TrackedAny, attempts: int = 0) -> Li
         cust_id = getattr(tracked_op, "cust_id", str(unknown_cust_id))
         cust_id = str(unknown_cust_id) if not cust_id else cust_id
         logger.info(f"{'=*=' * 20}")
+        logger.info(tracked_op.log_str)
         logger.info(f"{tracked_op.op_type} processing tracked operation {tracked_op.short_id}")
         logger.info(f"{tracked_op.log_str}")
         logger.info(
@@ -177,8 +178,8 @@ async def process_tracked_event(tracked_op: TrackedAny, attempts: int = 0) -> Li
                 tracked_op.process_time = process_time
                 await tracked_op.save()
                 logger.info(f"{'+++' * 10} {cust_id} {'+++' * 10}")
+                logger.info(tracked_op.log_str)
                 logger.info(f"{process_time:>7,.2f} s {cust_id} processing tracked operation")
-                logger.info(f"{tracked_op.log_str}")
                 logger.info(f"{'+++' * 10} {cust_id} {'+++' * 10}")
 
 
