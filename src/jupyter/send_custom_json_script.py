8809643deb2eafd7bbfd6b366c72c99fd7c7828d
Brian of London (home imac) <brian@v4v.app>
2026-02-14 14:55:29 +0200

    fix: Add database connection setup in main function and streamline custom JSON transfer process
    Signed-off-by: Brian of London (home imac) <brian@v4v.app>

--- src/jupyter/send_custom_json_script.py
+++ src/jupyter/send_custom_json_script.py
@@ -8,6 +8,7 @@ from nectar.amount import Amount
 
 import v4vapp_backend_v2.lnd_grpc.lightning_pb2 as lnrpc
 from v4vapp_backend_v2.config.setup import HiveRoles, InternalConfig, logger
+from v4vapp_backend_v2.database.db_pymongo import DBConn
 from v4vapp_backend_v2.helpers.crypto_conversion import CryptoConversion
 from v4vapp_backend_v2.helpers.crypto_prices import Currency
 from v4vapp_backend_v2.hive.hive_extras import (
@@ -76,45 +77,31 @@ async def main():
     """
     Main function to run the checks and print results.
     """
+    db_conn = DBConn()
+    await db_conn.setup_database()
+    # Deposit Hive as Keepsats
 
-    # # Deposit Hive as Keepsats
+    trx = await send_hive_customer_to_server(
+        amount=Amount("25 HIVE"), memo="Deposit and more #sats", customer="v4vapp-test"
+    )
+    pprint(trx)
 
-    # trx = await send_hive_customer_to_server(
-    #     amount=Amount("25 HIVE"), memo="Deposit and more #sats", customer="v4vapp-test"
+    # Send Sats back to this node.
+    # invoice = await get_lightning_invoice(
+    #     5010, "v4vapp.qrc #v4vapp Sending sats to another account"
     # )
-    # pprint(trx)
-
-    # # Send Sats back to this node.
-    # # invoice = await get_lightning_invoice(
-    # #     5010, "v4vapp.qrc #v4vapp Sending sats to another account"
-    # # )
-    # invoice = await get_lightning_invoice(2312, "v4vapp.qrc #v4vapp Sending sats to another account")
-    # # the invoice_message has no effect if the invoice is generated and sent in the message.
-    # # It is only used when the invoice is generated lightning_address
-    # # Sats amount is the amount to send for a 0 value invoice OR the maximum amount to send
-    # transfer = KeepsatsTransfer(
-    #     from_account="v4vapp-test",
-    #     sats=2312,
-    #     memo=invoice.payment_request,
-    #     invoice_message="v4vapp.qrc #v4vapp Sending sats to another account",
-    # )
-    # # hive_config = InternalConfig().config.hive
-    # hive_client = await get_verified_hive_client_for_accounts([transfer.from_account])
-    # trx = await send_custom_json(
-    #     json_data=transfer.model_dump(exclude_none=True, exclude_unset=True),
-    #     send_account=transfer.from_account,
-    #     active=True,
-    #     id="v4vapp_dev_transfer",
-    #     hive_client=hive_client,
-    # )
-    # pprint(trx)
-
-    # Transfer from test to qrc
+    invoice = await get_lightning_invoice(
+        2312, "brianoflondon #v4vapp Sending sats to another account"
+    )
+    # the invoice_message has no effect if the invoice is generated and sent in the message.
+    # It is only used when the invoice is generated lightning_address
+    # Sats amount is the amount to send for a 0 value invoice OR the maximum amount to send
     transfer = KeepsatsTransfer(
-        to_account="v4vapp-test",
-        from_account="v4vapp.qrc",
-        msats=5500113,
-        memo="back atcha",
+        from_account="v4vapp-test",
+        to_account="devser.v4vapp",
+        sats=2312,
+        memo=invoice.payment_request,
+        invoice_message="brianoflondon #v4vapp Sending sats to another account",
     )
     # hive_config = InternalConfig().config.hive
     hive_client = await get_verified_hive_client_for_accounts([transfer.from_account])
@@ -126,6 +113,24 @@ async def main():
         hive_client=hive_client,
     )
     pprint(trx)
+
+    # # Transfer from test to qrc
+    # transfer = KeepsatsTransfer(
+    #     to_account="v4vapp-test",
+    #     from_account="v4vapp.qrc",
+    #     msats=5500113,
+    #     memo="back atcha",
+    # )
+    # # hive_config = InternalConfig().config.hive
+    # hive_client = await get_verified_hive_client_for_accounts([transfer.from_account])
+    # trx = await send_custom_json(
+    #     json_data=transfer.model_dump(exclude_none=True, exclude_unset=True),
+    #     send_account=transfer.from_account,
+    #     active=True,
+    #     id="v4vapp_dev_transfer",
+    #     hive_client=hive_client,
+    # )
+    # pprint(trx)
 
 
 if __name__ == "__main__":
