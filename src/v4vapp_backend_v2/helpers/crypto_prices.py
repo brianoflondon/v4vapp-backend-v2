fa901fc399e1c5f629a34533ab1cd136570bcf00
Brian of London (home imac) <brian@v4v.app>
2026-04-11 17:33:23 +0300

    fix: add 'pymethod' to cSpell words and change log level to debug for database store skipping
    Signed-off-by: Brian of London (home imac) <brian@v4v.app>

--- src/v4vapp_backend_v2/helpers/crypto_prices.py
+++ src/v4vapp_backend_v2/helpers/crypto_prices.py
@@ -789,7 +789,7 @@ class AllQuotes(BaseModel):
             < timedelta(seconds=DB_RATES_MIN_INTERVAL)
         ):
             delta = format_time_delta(datetime.now(tz=timezone.utc) - AllQuotes.db_store_timestamp)
-            logger.info(f"{ICON} Skipping database store, last store was {delta} ago")
+            logger.debug(f"{ICON} Skipping database store, last store was {delta} ago")
             return record
         try:
             AllQuotes.db_store_timestamp = self.fetch_date
