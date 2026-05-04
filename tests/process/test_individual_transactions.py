c8673dfee5973777ec7da6a2bac65d9c7ec83557
Brian of London (home imac) <brian@v4v.app>
2026-05-04 11:18:09 +0300

    feat: Update memo formatting in Magisats transaction tests for clarity
    Signed-off-by: Brian of London (home imac) <brian@v4v.app>

--- tests/process/test_individual_transactions.py
+++ tests/process/test_individual_transactions.py
@@ -621,7 +621,7 @@ async def test_receive_magisats_inbound_payment_to_lightning_invoice_and_failure
     vsc_payload = VSCCallPayload(
         amount=str(1300),
         to=AccName(server_id).magi_prefix,
-        memo=f"{invoice_pr} #v4vapp #magioutbound #paywithsats:1300",
+        memo=f"{invoice_pr} | first attempt | #v4vapp #magioutbound #paywithsats:1300",
     )
     trx = await send_magi_transaction(
         vsc_payload=vsc_payload, nobroadcast=False, caller="v4vapp-test"
@@ -634,7 +634,7 @@ async def test_receive_magisats_inbound_payment_to_lightning_invoice_and_failure
     vsc_payload = VSCCallPayload(
         amount=str(1300),
         to=AccName(server_id).magi_prefix,
-        memo=f"{invoice_pr} #v4vapp #magioutbound #paywithsats:1300",
+        memo=f"{invoice_pr} | Second attempt to send | #v4vapp  #magioutbound #paywithsats:1300",
     )
     trx = await send_magi_transaction(
         vsc_payload=vsc_payload, nobroadcast=False, caller="v4vapp-test"
